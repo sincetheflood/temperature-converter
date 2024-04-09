4d8ccdc3370d427319de8664d6dd9f6ea476c45d
Ophelia Rozniak <dev@ophelia.ink>
2024-04-09 16:10:21 -0500

    remove code relating to slash commands

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,11 +1,9 @@
 import { Events } from "discord.js"
-import { Commands } from "../commands/index.js";
 
 export const ready = {
   name: Events.ClientReady,
   once: true,
   async execute(client) {
-    await client.application.commands.set(Commands);
 
     console.log(`Ready! Logged in as ${client.user.tag}`);
   },
--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
-import { Client, Collection, GatewayIntentBits } from "discord.js";
-import { Commands } from "./commands/index.js";
+import { Client, GatewayIntentBits } from "discord.js";
 import { Events } from "./events/index.js";
 import data from "../config.json" with { type: "json" };
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
-
-// Register commands
-client.commands = new Collection()
-
-for (let command of Commands) {
-  client.commands.set(command.name, command);
-}
 
 // Register events
 for (let event of Events) {
